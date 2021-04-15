8141595869797cfd4158eba8b44d0d47a47530b9
Eric Silva <ericdouglas1990@gmail.com>
2021-04-14 21:52:21 -0300

    criamos a classe page

---
+++ views/cache/index.52d47d6c7f5de5527c39e6860dc651af.rtpl.php
@@ -0,0 +1,10 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Home Page</h1>
+<form>
+	Nome: <br>
+	<input type="text" name=""><br><br>
+
+	Sobrenome: <br>
+	<input type="text" name=""><br><br>
+
+	<input type="submit" value="Enviar">
+</form>
